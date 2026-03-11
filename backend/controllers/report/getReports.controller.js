d4d3589f10a8b0b30ebd28b94462548ef30c8e87
Ori <odmark2023@gmail.com>
2026-03-11 09:50:39 +0200

    get reports with query complete

--- backend/controllers/report/getReports.controller.js
+++ backend/controllers/report/getReports.controller.js
@@ -1,35 +1,44 @@
 import Report from "../../models/reports.model.js";
 
 
-function filterArray(reports, query){
-    const keysQuery = Object.keys(query);
-    const filterQuery = reports.filter((report) => {
-        keysQuery.map(k => {
-        report.k === query.k
-        })
-    });
-    return filterQuery
-}
+// function filterArray(reports, query){
+//     const keysQuery = Object.keys(query);
+//     const filterQuery = reports.filter((report) => {
+//         keysQuery.map(k => {
+//         report[k] === query[k]
+//         })
+//     });
+//     return filterQuery
+// }
 async function getReports(req, res){
     try {
-        const user = req.user;
-        console.log(req.query)
-        const {category, urgency} = req.query;
-        console.log(urgency)
-        console.log(category)
-        const reports = await Report.find();
+        const {role, id} = req.user;
+        const {catagory, urgancy} = req.query;
+        console.log(role)
+        console.log(id)
+        console.log(catagory)
+        console.log(urgancy)
 
-        if(!reports) {
+        const filterReports = {};
+
+        if (catagory) {
+            filterReports.catagory = catagory;
+        }
+        if (urgancy) {
+            filterReports.urgancy = urgancy;
+        }
+
+        if (role === 'agent') {
+            console.log('hi agent')
+            filterReports.userId = id;
+            console.log(filterReports)
+        }
+        const reports = await Report.find(filterReports);
+
+        if(!reports || reports.length === 0) {
             return res.status(400).json({message: 'reports not found'})
         }
-        if(user.role === 'admin'){
-            console.log('give the admin every thing he wants and filter by the query')
-        } 
-        if (user.role === 'agent') {
-            const final = reports.filter(report => report.userId === user.id)
-            console.log('filter by the user id and by the query')
-            return res.status(200).json({message: 'report/s was found', Report: final})
-        }
+
         return res.status(200).json({message: 'reports was found', Report: reports})
         
     } catch (error) {
